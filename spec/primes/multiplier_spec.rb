8cd45e52db2d2182ea42bd2ec44c839196db4a7b
Ikenna Okpala <me@ikennaokpala.com>
2015-08-03 01:34:07 +0100

    Multiplier spec (thoughts)

--- spec/primes/multiplier_spec.rb
+++ spec/primes/multiplier_spec.rb
@@ -1,2 +1,13 @@
 describe Primes::Multiplier do
+  context 'when prime-numbers' do
+      describe 'multipling each other' do
+
+        it 'produces 10 number of elements' do
+          expect(false).to eq(true)
+        end
+
+        it 'returns products' do
+        end
+      end
+  end
 end
