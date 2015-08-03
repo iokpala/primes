efb22a59541e80fd1a13589bc212a1d58a89c62a
Ikenna Okpala <me@ikennaokpala.com>
2015-08-03 02:08:41 +0100

    Define and delegate to Table class composition of multiplication table data structure

--- lib/primes/table.rb
+++ lib/primes/table.rb
@@ -1,4 +1,51 @@
-module  Primes
+module Primes
   class Table
+
+    def initialize(multiplier)
+      @factors = multiplier.factors
+      @products = multiplier.products
+    end
+
+    def print
+       puts build
+    end
+
+  protected
+
+    def row(cells)
+      cells.map{|cell| "%-#{padding}s " % cell }
+    end
+
+    def dashes
+      "#{"-"*(padding+1)}+­#{"-"*(factor_cells_x.join.size-padding)}"
+    end
+
+    def head_row
+      " "*(padding+1) + factor_cells_x.join
+    end
+
+    def factor_cells_x
+      @factor_cells_x ||= row(["|"]+@factors)
+    end
+
+    def factor_cells_y
+      @factor_cells_y ||= @factors.zip(["|"]*@factors.size)
+    end
+
+  private
+
+    def padding
+      @padding ||= @products.flatten.max.to_s.split(//).count + 1
+    end
+
+    def build
+      [].tap do |rows|
+        rows << head_row
+        rows << dashes
+        @products.each_with_index do |product_cells, index|
+          rows << row(factor_cells_y[index]+product_cells).join
+        end
+      end
+    end
   end
 end
--- spec/primes/table_spec.rb
+++ spec/primes/table_spec.rb
@@ -1,11 +1,37 @@
 require 'spec_helper'
 
 describe Primes::Table do
-  context 'can build table' do
+  context 'table building' do
     describe "for n number prime-numbers" do
       let(:n) { 10 }
       let(:multiplier) { Primes::Multiplier.new(n) }
-      it{}
+      let(:table) { Primes::Table.new(multiplier) }
+      let(:padding) { table.send(:padding) }
+      subject { table.send(:build) }
+      let(:head){"|    2    3    5    7    11   13   17   19   23   29"}
+      let(:dashes){"-----+­---------------------------------------------------"}
+
+      it 'containing n+2 rows' do
+        expect(subject.count).to eq(n+2)
+      end
+
+      it 'containing heading row' do
+        expect(subject.first.strip).to eq(head)
+      end
+
+      it 'containing dashes row' do
+        expect(subject[1].strip).to eq(dashes)
+      end
+
+      it 'with whitespace padding equal to character length of the highest product' do
+        expect(subject.last.strip.split.last.length).to eq(3)
+      end
+
+      it 'containing one pipe, whitespaces and numbers in each cell' do
+        subject.drop(2).each do |cell|
+          expect(cell).to match(/^\d+\s+\|\s+(\d+\s+)+\d+\s+$/)
+        end
+      end
     end
   end
 end
